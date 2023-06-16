3dbf80f11ca720b3eed30717b0b6078d473dc6a4
jlh425 <105890316+jlh425@users.noreply.github.com>
2023-06-16 06:42:45 -0600

    updated card

--- src/components/NewsCards/NewsCard/NewsCard.js
+++ src/components/NewsCards/NewsCard/NewsCard.js
@@ -1,11 +1,36 @@
 import React from 'react'
-
-const NewsCard = () => {
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@mui/material'
+import {news} from "../assets/images/news.svg";
+import useStyles from "./styles";
+const NewsCard = ({article: {description, publishedAt, Source, title, url, urlToImage}, i}) => {
+  const classes = useStyles();
   return (
-    <div>
-        News Card
-    </div>
-  )
+    <Card>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={urlToImage || news }  />
+        <div>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          >{(new Date(publishedAt)).toDateString()}</Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          >{Source.name}</Typography>
+        </div>
+        <Typography gutterBottom variant="h5"></Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" components="p" >{description}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">Learn More</Button>
+        <Typography variant='h5' color="textSecondary">{i +1}</Typography>
+      </CardActions>
+    </Card>
+  );
 }
 
 export default NewsCard
